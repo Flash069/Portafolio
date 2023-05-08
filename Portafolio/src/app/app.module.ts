762b1656b9c2d9cd0a4bb97fb4cfc790a5e6bb30
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-08 18:00:53 -0300

    FinFrontEnd

--- Portafolio/src/app/app.module.ts
+++ Portafolio/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { InfopersonalComponent } from './componentes/infopersonal/infopersonal.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './componentes/footer/footer.component' ;
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesSocialesComponent,
     BannerComponent,
     InfopersonalComponent,
     ExperienciaComponent,
