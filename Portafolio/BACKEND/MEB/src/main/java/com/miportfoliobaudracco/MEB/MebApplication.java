09423eabea9f2b19c345d3bd3bcc2c98cdff22f2
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-08 19:40:02 -0300

    NEWBACKENDJAVA17

---
+++ Portafolio/BACKEND/MEB/src/main/java/com/miportfoliobaudracco/MEB/MebApplication.java
@@ -0,0 +1,13 @@
+package com.miportfoliobaudracco.MEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MebApplication.class, args);
+	}
+
+}
