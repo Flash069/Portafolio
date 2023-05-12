ecb4a8ba00f7cb226031db9ba0ccec87dbd6a8cc
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-12 19:32:34 -0300

    traernombreuser

--- Portafolio/BACKEND/MEB/src/main/java/com/miportfoliobaudracco/MEB/Controlador/Usuariocontrol.java
+++ Portafolio/BACKEND/MEB/src/main/java/com/miportfoliobaudracco/MEB/Controlador/Usuariocontrol.java
@@ -8,6 +8,7 @@ import com.miportfoliobaudracco.MEB.Ente.Usuario;
 import com.miportfoliobaudracco.MEB.Interface.IUsuarioServicio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 /**
  *
  * @author Euge
@@ -55,5 +56,5 @@ return usuario;
 }
 @GetMapping("/usuario/traer/perfil")
 public Usuario findUsuario(){ 
-    return iusuarioServicio.findUsuario((Long)1);}
+    return iusuarioServicio.findUsuario((long)1);}
 }
