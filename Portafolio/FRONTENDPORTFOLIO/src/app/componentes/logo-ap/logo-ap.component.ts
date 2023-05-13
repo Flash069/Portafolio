89f609ac2da67ad7b59456a3540abd49fcbf6845
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-13 03:29:41 -0300

    cerrar sesion

--- Portafolio/FRONTENDPORTFOLIO/src/app/componentes/logo-ap/logo-ap.component.ts
+++ Portafolio/FRONTENDPORTFOLIO/src/app/componentes/logo-ap/logo-ap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/servicio/token.service';
 
 @Component({
   selector: 'app-logo-ap',
@@ -7,9 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./logo-ap.component.css']
 })
 export class LogoAPComponent implements OnInit {
-  constructor(private router: Router){}
-    ngOnInit(): void {}
+  isLogged = false;
 
+  constructor(private router: Router, private tokenService: TokenService){}
+    ngOnInit(): void {
+      if(this.tokenService.getToken()){
+        this.isLogged = true;
+
+      }
+      else {
+        this.isLogged =false;
+      }
+    }
+onLogOut(): void {
+  this.tokenService.logOut();
+  window.location.reload();
+  
+}
 ingreso(){
   this.router.navigate(['/ingreso'])
 }
