174cbb91f188e21b1e8b02293e85c9ef874c1b29
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-14 16:57:50 -0300

    usserer

---
+++ Portafolio/BACKEND/Portafolio/src/main/java/com/BACK/Portafolio/Security/JWT/JwtTokenFilter.java
@@ -0,0 +1,44 @@
+package com.BACK.Portafolio.Security.JWT;
+
+import com.BACK.Portafolio.Security.Service.UserDetailsImp;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.Null;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    UserDetailsImp userDetailsImp;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            if (token != null && jwtProvider.validateToken(token)) {
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsImp.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            logger.error("Falló el método doFilterInternal");
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String getToken(HttpServletRequest request){
+        Strinng header = request.getHeader(ALREADY_FILTERED_SUFFIX)
+    }
+            }
