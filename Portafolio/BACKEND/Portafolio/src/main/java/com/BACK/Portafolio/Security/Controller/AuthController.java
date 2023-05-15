241647ccdadfb102c13e511df1be517e2513d32d
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-14 22:36:12 -0300

    siinmain

--- Portafolio/BACKEND/Portafolio/src/main/java/com/BACK/Portafolio/Security/Controller/AuthController.java
+++ Portafolio/BACKEND/Portafolio/src/main/java/com/BACK/Portafolio/Security/Controller/AuthController.java
@@ -90,22 +90,18 @@ public class AuthController {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
         }
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(logginUsuario.getNombreUsuario(), logginUsuario.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+       
+       SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = jwtProvider.generateToken(authentication);
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-        JwtDto jwtDto = new jwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
         return new ResponseEntity(jwtDto, HttpStatus.OK);
     }
 
-    private static class jwtDto extends JwtDto {
-
-        public jwtDto( jwt, username, Collection<? extends GrantedAuthority> authorities) {
-        }
-    }
-
+    
    
 }
--- Portafolio/BACKEND/Portafolio/src/main/java/com/BACK/Portafolio/Security/MainSecurity.java
+++ Portafolio/BACKEND/Portafolio/src/main/java/com/BACK/Portafolio/Security/MainSecurity.java
@@ -4,22 +4,66 @@
  */
 package com.BACK.Portafolio.Security;
 
+import com.BACK.Portafolio.Security.JWT.JwtEntryPoint;
+import com.BACK.Portafolio.Security.JWT.JwtTokenFilter;
 import com.BACK.Portafolio.Security.Service.UserDetailsImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MainSecurity {
+
+
+public class MainSecurity 
+        //extends WebSecurityConfigurerAdapter
+{
+    
     @Autowired
     UserDetailsImp userDetailsServiceImp;
     @Autowired
     JwtEntryPoint jwtEntryPoint;
     
+  
     @Bean
-    public JwtTokeFilter jwtTokenFilterr(){
+    public JwtTokenFilter jwtTokenFilter(){
         return new JwtTokenFilter();
     }
+    
+//    @Bean
+//    public PasswordEncoder passwordEncoder()
+//    {
+//        return new BCryptPasswordEncoder(); }
+//    
 }
+
+//    private static class PasswordEncoder {
+//
+//        public PasswordEncoder() {
+//        }
+//    }
+    
+}
+    
+//// @Override
+////protected void configure(HttpSecurity http) throws Exception{
+////super.configure(http); 
+////}
+// // @Override
+//protected AuthenticationManager authenticationManager() throws Exception {
+//return super.authenticationManager(); 
+//}
+//
+//@Override
+//public AuthenticationManager authenticationManagerBean() throws Exception {
+//return super.authenticationManagerBean(); 
+//}
+//@Override
+//protected void configure(AuthenticationManagerBuilder auth) throws Exception ({
+//super.configure(auth); 
+//}
+//    
+//}
